86211878c8755d0f5bfd043b5d8204e886f89f4f
FranciscoBraga <franciscobraga.fob@gmail.com>
2024-10-08 11:14:39 -0400

    Instanciando inimigo e dano do jogador

--- script/arena.gd
+++ script/arena.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+var inimigo_1 = preload("res://inimigo.tscn")
  
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -7,3 +8,12 @@ func _ready() -> void:
 
 func _exit_tree() -> void:
 	Global.criacao_no_pai =  null
+
+func _on_timer_spawn_inimigo_timeout() -> void:
+	print("inimigo")
+	var posicao_inimigo = Vector2(randi_range(-160,670),randi_range(-90,390))
+	
+	while posicao_inimigo.x < 640 and posicao_inimigo.x > -80 and posicao_inimigo.y < 360 and posicao_inimigo.y > -45:
+		posicao_inimigo = Vector2(randi_range(-160,670),randi_range(-90,360))
+		
+		Global.instance_node(inimigo_1,posicao_inimigo,self)
--- script/jogador.gd
+++ script/jogador.gd
@@ -7,6 +7,8 @@ var projetil = preload("res://script/projetil.tscn")
 
 var recarregado = true
 
+var morto = false
+
 func _ready() -> void:
 	Global.jogador = self
 	
@@ -17,13 +19,23 @@ func _process(delta: float) -> void:
 	mov.x = int(Input.is_action_pressed("direita")) - int(Input.is_action_pressed("esquerda"))
 	mov.y = int(Input.is_action_pressed("baixo")) - int(Input.is_action_pressed("cima"))
 	
-	global_position += velocidade * mov * delta
+	if morto == false:
+		global_position += velocidade * mov * delta
 	
-	if Input.is_action_pressed("atirar") and Global.criacao_no_pai != null  and  recarregado:
+	
+	
+	if Input.is_action_pressed("atirar") and Global.criacao_no_pai != null  and  recarregado and morto == false:
 		Global.instance_node(projetil,global_position, Global.criacao_no_pai)
 		recarregado = false
 		$tempo_recarga.start()
 
 func _on_tempo_recarga_timeout() -> void:
 	recarregado = true
+	
+func _on_hitbox_area_entered(area: Area2D) -> void:
+	if area.is_in_group("inimigo"):
+		visible = false
+		morto = true
+		await get_tree().create_timer(1).timeout
+		get_tree().reload_current_scene()
 	
